41b9a4c72cc49792748d2f2dca7013b96adb44fc
Doorstep Experts Developer <dev@doorstepexperts.com>
2026-08-31 01:37:36 +0530

    Add Swiggy-Style Google Play Store Android native package files and APK install modal

---
+++ android/app/src/main/java/com/doorstepexperts/app/MainActivity.java
@@ -0,0 +1,43 @@
+package com.doorstepexperts.app;
+
+import android.os.Bundle;
+import android.webkit.WebSettings;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import androidx.appcompat.app.AppCompatActivity;
+
+public class MainActivity extends AppCompatActivity {
+
+    private WebView webView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        webView = new WebView(this);
+        setContentView(webView);
+
+        WebSettings webSettings = webView.getSettings();
+        webSettings.setJavaScriptEnabled(true);
+        webSettings.setDomStorageEnabled(true);
+        webSettings.setDatabaseEnabled(true);
+        webSettings.setAllowFileAccess(true);
+        webSettings.setAllowContentAccess(true);
+        webSettings.setGeolocationEnabled(true);
+        webSettings.setCacheMode(WebSettings.LOAD_DEFAULT);
+
+        webView.setWebViewClient(new WebViewClient());
+
+        // Load Doorstep Experts app
+        webView.loadUrl("file:///android_asset/doorstep_experts.html");
+    }
+
+    @Override
+    public void onBackPressed() {
+        if (webView.canGoBack()) {
+            webView.goBack();
+        } else {
+            super.onBackPressed();
+        }
+    }
+}
